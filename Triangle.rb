8eb28ecb5e1324bafe4648dd8a70afb03961ee35
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-06 08:41:48 -0800

    Initial Commit

---
+++ Triangle.rb
@@ -0,0 +1,11 @@
+=begin
+Triangle class
+Store:
+  User is expected to provide:
+    length of the three sides
+  Class should provide:
+    perimeter (sum of the sides)
+    area
+    angles
+    ?three "heights", i.e. right-angle distance from each vertex to the base
+=end
