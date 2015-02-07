2ef1117be738e8308f309780dca9f887bd12194a
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-07 15:35:33 -0800

    Update structure of Triangle class

--- Triangle.rb
+++ Triangle.rb
@@ -11,51 +11,53 @@ Store:
 =end
 
 class Triangle
-  attr_reader :sides
+  attr_reader :side1, :side2, :side3
   def initialize(lengths)
-    @sides = storeSides(lengths)
+    @side1 = lengths[:side1]
+    @side2 = lengths[:side2]
+    @side3 = lengths[:side3]
+
     if (!triangleIsPossible)
       puts "Triangle cannot be created!"
       puts "The sum of the lengths any two sides must be greater than length of the third."
-      @sides = storeSides([0,0,0])
+      @side1 = 0
+      @side2 = 0
+      @side3 = 0
     end
   end
 
   def showSides
-    puts "side1:#{sides.side1}, side2:#{sides.side2}, side3:#{sides.side3}"
+    puts "side1:#{side1}, side2:#{side2}, side3:#{side3}"
   end
 
   def perimiter
-    @perimiter = sides.side1 + sides.side2 + sides.side3
+    @perimiter = side1 + side2 + side3
   end
 
   def area
     #Heron's formula
     p = perimiter
-    @area = Math.sqrt(p*(p-sides.side1)*(p-sides.side2)*(p-sides.side3))
+    @area = Math.sqrt(p*(p-side1)*(p-side2)*(p-side3))
   end
 
   def triangleIsPossible
-    if (sides.side1 + sides.side2 <= sides.side3)
+    if (side1 + side2 <= side3)
       return false
     end
-    if (sides.side1 + sides.side3 <= sides.side2)
+    if (side1 + side3 <= side2)
       return false
     end
-    if (sides.side2 + sides.side3 <= sides.side1)
+    if (side2 + side3 <= side1)
       return false
     end
     return true
   end
-
-  TriangleSides = Struct.new(:side1, :side2, :side3)
-  def storeSides(lengths)
-    TriangleSides.new(lengths[0], lengths[1], lengths[2])
-  end
 end
 
-t = Triangle.new([10,25,30])
-puts t.sides
+t = Triangle.new(
+  :side1 => 10,
+  :side2 => 25,
+  :side3 => 30)
 t.showSides
 puts t.perimiter
 puts t.area
